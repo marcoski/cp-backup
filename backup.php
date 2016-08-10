76f613a392b3b047958f04aa7050116be396d12e
marcoski <mtrognon@gmail.com>
2016-08-10 10:39:36 +0200

    1.0.dev - adding phar compiling configuration

--- backup.php
+++ backup.php
@@ -5,7 +5,7 @@ use Backup\Command\BackupCommand;
 use Backup\Command\RemoteBackupCommand;
 
 require_once 'vendor/autoload.php';
-$application = new Application('CpBackup', '1.0.dev');
+$application = new Application('CpBackup', '@package_version@');
 $application->add(new BackupCommand());
 $application->add(new RemoteBackupCommand());
 $application->run();
